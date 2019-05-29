c2d91b01152415d2772344d5dd330e88efb1258e
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2019-05-29 15:23:55 +0200

    removing some files

---
+++ R-scripts/02_imager_input_Joey.R
@@ -0,0 +1,24 @@
+
+
+
+library(tidyverse)
+library(cowplot)
+library(readxl)
+
+
+
+RFU_files <- c(list.files("data-raw/imager-outputs", full.names = TRUE))
+RFU_files <- RFU_files[grepl(".xls", RFU_files)]
+
+names(RFU_files) <- RFU_files %>% 
+  gsub(pattern = ".xlsx$", replacement = "") %>% 
+  gsub(pattern = ".xls$", replacement = "")
+
+# RFU_files[grepl("104", RFU_files)]
+
+# RFU_files <- RFU_files[!grepl("acc", RFU_files)]
+
+all_plates <- map_df(RFU_files, read_excel, range = "B56:N64", .id = "file_name") %>%
+  rename(row = X__1) %>% 
+  filter(!grepl("dilution", file_name)) %>% 
+  mutate(file_name = str_replace(file_name, " ", ""))
