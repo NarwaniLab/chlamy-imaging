ce467f4233d741a6012505b3030da015ed4a9eb0
Lenka Rihakova <lenka.rihakova@etu.unistra.fr>
2019-06-13 20:27:26 +0200

    changing names

--- BF_phosphate.R
+++ BF_phosphate.R
@@ -4,14 +4,33 @@ library (cowplot)
 library (plotrix)
 library (janitor)
 
-BF_files <- c(list.files("data-raw/results_BF_phosphate_plate_10", full.names = TRUE))
-BF_files <- BF_files[grepl(".csv", BF_files)]
+plate_layout <- read_excel("data-raw/chlamee-acclimated-plate-layout.xlsx") 
+plate_info <- read_csv("data-raw/chlamee-phosphate-rstar-plate-info.csv")
 
-four_wells <- map_df(BF_files, read_csv, .id = "file_name") %>% 
+treatments <- read_excel("data-general/ChlamEE_Treatments_JB.xlsx") %>% 
+  clean_names() %>% 
+  mutate(treatment = ifelse(is.na(treatment), "none", treatment)) 
+
+BF_files <- c(list.files("data-raw/results_BF_ph_plate_19", full.names = TRUE))
+BF_files <- BF_files[grepl(".csv", BF_files)] 
+
+all_wells <- map_df(BF_files, read_csv, .id = "file_name") %>% 
   rename(cell_number = X1) %>% 
-  mutate(file_name = str_replace(file_name, " ", "")) %>% 
   separate(col = file_name, into = c("file_name", "plate"), 
-           sep = c("phosphate_"))
+           sep = c("phosphate_")) %>% 
+  mutate(plate = "19") 
+
+all_wells2 <- all_wells %>% 
+  unite(cell_number, Area, col = "well", remove = FALSE, sep = "") %>% 
+  
+ 
+all_BF <- left_join(all_wells, plate_info, by = c("well", "plate")) %>%
+  mutate(plate = as.numeric(plate)) %>% 
+  left_join(., treatments) %>% 
+  filter(population != "COMBO")
+
+all_BF2 <- all_BF %>% 
+  group_by(cell_number, Area, treatment, ancestor_id)
 
 four_wells %>% 
   ggplot(aes(x = Area, y = cell_number, color = file_name)) + geom_point() +
