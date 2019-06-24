7ae3a57116577ec1c643e4eb6338aeb16469b5cc
Lenka Rihakova <lenka.rihakova@etu.unistra.fr>
2019-06-24 15:30:25 +0200

    adding new scrits

---
+++ R-scripts/chlA_cell_count.R
@@ -0,0 +1,55 @@
+library (tidyverse)
+library (readxl)
+library (cowplot)
+library (plotrix)
+library (janitor)
+
+plate_layout <- read_excel("data-raw/chlamee-acclimated-plate-layout.xlsx") 
+plate_info <- read_csv("data-raw/chlamee-phosphate-rstar-plate-info.csv")
+
+treatments <- read_excel("data-general/ChlamEE_Treatments_JB.xlsx") %>% 
+  clean_names() %>% 
+  mutate(treatment = ifelse(is.na(treatment), "none", treatment)) 
+
+fluo_files <- c(list.files("data-raw/results_chlA_ph_plate_28", full.names = TRUE))
+fluo_files <- fluo_files[grepl(".csv", fluo_files)] 
+fluo_files[3] 
+
+names(fluo_files) <- fluo_files %>% 
+  gsub(pattern = ".csv$", replacement = "")
+
+all_wells <- map_df(fluo_files, read_csv, .id = "file_name") %>%
+  rename(cell_count = X1) %>%
+  separate(col = file_name, into = c("file_name", "plate"), 
+           sep = "plate_28/") %>%
+  #separate(col = plate, into = c("well", "plate"), 
+  #sep = c("01_1_1_BrightField_001_results_plate","01_1_2_BrightField_001_results_plate")) %>% 
+  mutate(plate = str_replace(plate, "01_1_1_Chlorophyll_A_001_plate_", "x")) %>%
+  mutate(plate = str_replace(plate, "01_1_2_Chlorophyll_A_001_plate_", "x")) %>% 
+  separate(plate, into = c("well", "plate"),
+           sep = "_x") %>%
+  mutate(plate = as.numeric(plate)) %>% 
+  separate(col = well, into = c("row", "column"),
+           sep = 1) %>%
+  mutate(column = formatC(column, width = 2, flag = 0)) %>% 
+  mutate(column = str_replace(column, " ", "0")) %>% 
+  unite(col = well, row, column, sep = "")
+
+all_wells2 <- left_join(all_wells, plate_info, by = c("well", "plate")) %>%
+  mutate(plate = as.numeric(plate)) %>% 
+  filter(!is.na(plate_key)) %>% 
+  left_join(., treatments) %>% 
+  filter(population != "COMBO")
+
+#all_wells2 %>% 
+  #filter(well == "B10") 
+
+all_BF <- all_wells2 %>% 
+  group_by(population, phosphate_concentration, treatment, ancestor_id, well) %>%
+  summarise_each(funs(max), cell_count)
+
+all_BF %>% 
+  ggplot(aes(x = treatment, y = cell_count, color = phosphate_concentration)) + geom_point() +
+  scale_color_viridis_c()   
+
+
